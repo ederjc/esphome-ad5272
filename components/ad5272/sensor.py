d9cc98dd1d635386880978732a7e9eee92c1bb17
Eder Julian <Julian.Eder@infineon.com>
2025-10-30 22:06:08 +0100

    4th copilot iteration

--- components/ad5272/sensor.py
+++ components/ad5272/sensor.py
@@ -15,22 +15,10 @@ AD5272Sensor = ad5272_ns.class_('AD5272Sensor', sensor.Sensor, cg.Component)
 
 CONF_TYPE = 'type'
 
-SENSOR_TYPES = {
-    'resistance': {
-        'icon': 'mdi:resistor',
-        'unit': UNIT_OHM,
-        'accuracy_decimals': 1,
-        'device_class': DEVICE_CLASS_EMPTY,
-        'state_class': STATE_CLASS_MEASUREMENT,
-    },
-    'wiper_position': {
-        'icon': 'mdi:tune-vertical',
-        'unit': '',
-        'accuracy_decimals': 0,
-        'device_class': DEVICE_CLASS_EMPTY,
-        'state_class': STATE_CLASS_MEASUREMENT,
-    },
-}
+SENSOR_TYPE_RESISTANCE = "resistance"
+SENSOR_TYPE_WIPER_POSITION = "wiper_position"
+
+SENSOR_TYPES = [SENSOR_TYPE_RESISTANCE, SENSOR_TYPE_WIPER_POSITION]
 
 CONFIG_SCHEMA = sensor.sensor_schema(
     AD5272Sensor,
@@ -39,7 +27,7 @@ CONFIG_SCHEMA = sensor.sensor_schema(
     state_class=STATE_CLASS_MEASUREMENT,
 ).extend({
     cv.GenerateID(CONF_AD5272_ID): cv.use_id(AD5272Component),
-    cv.Required(CONF_TYPE): cv.enum(SENSOR_TYPES, lower=True),
+    cv.Required(CONF_TYPE): cv.one_of(*SENSOR_TYPES, lower=True),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -50,11 +38,14 @@ async def to_code(config):
     cg.add(var.set_parent(parent))
     
     sensor_type = config[CONF_TYPE]
-    cg.add(var.set_sensor_type(cg.const_char_ptr(sensor_type)))
+    cg.add(var.set_sensor_type(sensor_type))
     
-    # Apply sensor type specific settings
-    type_config = SENSOR_TYPES[sensor_type]
-    if 'icon' in type_config and 'icon' not in config:
-        cg.add(var.set_icon(type_config['icon']))
-    if 'unit' in type_config and type_config['unit'] and 'unit_of_measurement' not in config:
-        cg.add(var.set_unit_of_measurement(type_config['unit']))
+    # Apply sensor type specific settings based on type
+    if sensor_type == SENSOR_TYPE_RESISTANCE:
+        if 'icon' not in config:
+            cg.add(var.set_icon('mdi:resistor'))
+        if 'unit_of_measurement' not in config:
+            cg.add(var.set_unit_of_measurement(UNIT_OHM))
+    elif sensor_type == SENSOR_TYPE_WIPER_POSITION:
+        if 'icon' not in config:
+            cg.add(var.set_icon('mdi:tune-vertical'))
